783109eefbbd3ce1525022e8aa1cc1fef6cd3155
clesiemo3 <clesiemo3@gmail.com>
2016-10-22 16:13:00 -0500

    access control who needs it

--- user/user.go
+++ user/user.go
@@ -36,10 +36,10 @@ func CreateUser(w http.ResponseWriter, req *http.Request) {
     /*(if req.Method != "POST"{
       return
     }*/
-    if origin := req.Header.Get("Origin"); origin != "" {
-        w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-        w.Header().Set("Access-Control-Allow-Headers",
-            "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+    w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
+    w.Header().Set("Access-Control-Allow-Headers",
+            "Accept, Content-Type, Content-Length, Accept-Encoding,
+            X-CSRF-Token, Authorization")
     }
     // Stop here if its Preflighted OPTIONS request
     if req.Method == "OPTIONS" {
