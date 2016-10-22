a68db256f6b3efbc5b9c6fed1caba6edb997ac49
clesiemo3 <clesiemo3@gmail.com>
2016-10-22 16:13:44 -0500

    im bad

--- user/user.go
+++ user/user.go
@@ -38,8 +38,7 @@ func CreateUser(w http.ResponseWriter, req *http.Request) {
     }*/
     w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
     w.Header().Set("Access-Control-Allow-Headers",
-            "Accept, Content-Type, Content-Length, Accept-Encoding,
-            X-CSRF-Token, Authorization")
+            "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
     }
     // Stop here if its Preflighted OPTIONS request
     if req.Method == "OPTIONS" {
