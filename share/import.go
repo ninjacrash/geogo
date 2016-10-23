9a3adcf2c8aa436f437980ace8193a2981eade24
Nathan McGinn <nathan.mcginn@climate.com>
2016-10-22 22:14:09 -0500

    imports working, duplicate IDs may need to be filtered out to allow auto increment to kick in

--- share/import.go
+++ share/import.go
@@ -5,9 +5,11 @@ import(
 	"net/http"
 	"encoding/json"
 	"io/ioutil"
+	"bytes"
 )
 
 func DataImport(w http.ResponseWriter, r *http.Request) {
+	base_url := "http://pg.globalhack.ninja/"
 	js, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
@@ -22,6 +24,13 @@ func DataImport(w http.ResponseWriter, r *http.Request) {
 	}
 
 	for _, meeseek := range life_is_pain_to_a_meeseeks {
-		fmt.Fprintf(w, "%s", meeseek.Schema)
+		api_url := base_url + meeseek.Name
+		fmt.Printf(api_url)
+		resp, err := http.Post(api_url, "application/json", bytes.NewBuffer([]byte(meeseek.Data)))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprintf(w, "%s", resp)
 	}
 }
