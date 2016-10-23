c923d61161bd5595af4f620724846079c1bfb5d5
Ubuntu <ubuntu@ip-10-0-0-48.ec2.internal>
2016-10-23 12:46:27 +0000

    fix copy

--- pull-all.sh
+++ pull-all.sh
@@ -4,8 +4,8 @@
 #git pull
 cd ~/workspace/src/github.com/ninjacrash/geogo
 git pull
-cp -rf frontend ~/frontend
-cp -rf frontend_org ~/frontend_org
+cp -rf frontend ~
+cp -rf frontend_org ~
 pkill -f geogo
 nohup go run geogo.go &
 echo $!
