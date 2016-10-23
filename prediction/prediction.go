8e357d8ca864090d35d56386afebd7c562349ccc
Nathan McGinn <nathan.mcginn@climate.com>
2016-10-23 01:26:00 -0500

    whoop

--- prediction/prediction.go
+++ prediction/prediction.go
@@ -6,15 +6,16 @@ import(
 	"os/exec"
 	"bytes"
 	"log"
+	"io/ioutil"
 )
 
 func Predict(w http.ResponseWriter, r *http.Request) {
 	js, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
-	cmd := exec.Command("Rscript", "live_prediction.R", js)
+	cmd := exec.Command("Rscript", "live_prediction.R", string(js))
 	var out bytes.Buffer
 	cmd.Stdout = &out
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
