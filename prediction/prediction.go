ecd094edf24f47584a51189f9ef2e988f7f0f66d
clesiemo3 <clesiemo3@gmail.com>
2016-10-23 06:10:27 -0500

    options

--- prediction/prediction.go
+++ prediction/prediction.go
@@ -9,7 +9,7 @@ import(
 )
 
 func Predict(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-type")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	// Stop here if its Preflighted OPTIONS request
