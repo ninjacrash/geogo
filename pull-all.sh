f774ba39e74aa0e8fe7e9a8fc0968004b19609d5
Ubuntu <ubuntu@ip-10-0-0-48.ec2.internal>
2016-10-23 11:09:33 +0000

    order matters

--- pull-all.sh
+++ pull-all.sh
@@ -1,11 +1,11 @@
-cd ~/frontend
-git pull
-cd ~/frontend_org
-git pull
+#cd ~/frontend
+#git pull
+#cd ~/frontend_org
+#git pull
 cd ~/workspace/src/github.com/ninjacrash/geogo
+git pull
 cp -rf frontend ~/frontend
 cp -rf frontend_org ~/frontend_org
-git pull
 pkill -f geogo
 nohup go run geogo.go &
 echo $!
